acadad2d4dacfba277567a1e71ffe6f34b84e4cf
Dj9162 <89787669+Dj9162@users.noreply.github.com>
2023-11-29 17:30:30 +0530

    quickSort.js

--- algorithms/quickSort.js
+++ algorithms/quickSort.js
@@ -2,49 +2,49 @@ const sleep = (time) => {
     return new Promise(resolve => setTimeout(resolve, time));
 };
 
-// Red - '#FF0000'
-// Green - '#00FF00'
-// Grey - '#AA9870'
-// Violet - '#BB0123'
-
 async function quickSort(arr, start, end, speed) {
     if (start < end) {
-        let pivot = arr[start];
-        let pivotIndex = start;
-        let e1 = document.getElementById('elem' + pivotIndex);
-        e1.style.backgroundColor = '#FF0000';
-        await sleep(speed);
-
-        for (let i = start + 1; i <= end; i++) {
-            let e2 = document.getElementById('elem' + i);
-            e2.style.backgroundColor = '#BB0123';
-            await sleep(speed);
-
-            if (arr[i] < pivot) {
-                pivotIndex++;
-                [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
-                await sleep(speed);
-                swapHeight(i, pivotIndex);
-                swapColor(i, pivotIndex);
-            }
-            e2.style.backgroundColor = '#AA9870';
-        }
-
-        [arr[start], arr[pivotIndex]] = [arr[pivotIndex], arr[start]];
-        swapHeight(start, pivotIndex);
-        swapColor(start, pivotIndex);
-        e1.style.backgroundColor = '#00FF00';
-
-        await sleep(speed);
-
+        let pivotIndex = await partition(arr, start, end, speed);
         await quickSort(arr, start, pivotIndex - 1, speed);
         await quickSort(arr, pivotIndex + 1, end, speed);
     } else {
         let e = document.getElementById('elem' + start);
-        e.style.backgroundColor = '#FF0000';
-        await sleep(speed);
         e.style.backgroundColor = '#AA9870';
+        await sleep(speed);
+        e.style.backgroundColor = '#FF0000';
     }
+}
+
+async function partition(arr, start, end, speed) {
+    let pivot = arr[start];
+    let pivotIndex = start;
+    let e1 = document.getElementById('elem' + pivotIndex);
+    e1.style.backgroundColor = '#FF0000';
+    await sleep(speed);
+
+    for (let i = start + 1; i <= end; i++) {
+        let e2 = document.getElementById('elem' + i);
+        e2.style.backgroundColor = '#BB0123';
+        await sleep(speed);
+
+        if (arr[i] < pivot) {
+            pivotIndex++;
+            [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+            await sleep(speed);
+            swapHeight(i, pivotIndex);
+            swapColor(i, pivotIndex);
+        }
+        e2.style.backgroundColor = '#AA9870';
+    }
+
+    [arr[start], arr[pivotIndex]] = [arr[pivotIndex], arr[start]];
+    swapHeight(start, pivotIndex);
+    swapColor(start, pivotIndex);
+    e1.style.backgroundColor = '#00FF00';
+
+    await sleep(speed);
+
+    return pivotIndex;
 }
 
 async function swapHeight(first, last) {
